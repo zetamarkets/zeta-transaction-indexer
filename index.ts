cdb4b76900158baefb6445c62956e124729af353
tfrizza <tfrizza@gmail.com>
2022-10-05 23:01:16 +0800

    parallel async tx fetching, exponential backoff for rpc with rpc fallback

--- index.ts
+++ index.ts
@@ -1,14 +1,121 @@
-import { Connection } from "@solana/web3.js";
+import {
+  Commitment,
+  ConfirmedSignatureInfo,
+  ConfirmedSignaturesForAddress2Options,
+  Connection,
+  ConnectionConfig,
+  Finality,
+  GetVersionedTransactionConfig,
+  ParsedTransactionWithMeta,
+  PublicKey,
+  SolanaJSONRPCError,
+  TransactionSignature,
+  VersionedTransactionResponse,
+} from "@solana/web3.js";
 import { getTxIndexMetadata, putTxIndexMetadata } from "./utils/s3";
 import { putFirehoseBatch } from "./utils/firehose";
 import {
   PROGRAM_ID,
   MAX_SIGNATURE_BATCH_SIZE,
   DEBUG_MODE,
+  MAX_TX_BATCH_SIZE,
 } from "./utils/constants";
 import { sleep } from "@zetamarkets/sdk/dist/utils";
 
-let connection = new Connection(process.env.RPC_URL, "finalized");
+class SolanaRPC {
+  nodeUrl;
+  commitmentOrConfig;
+  connection;
+  NODE_URL_1;
+  NODE_URL_2;
+  period;
+  MAX_NODE_THRESHOLD = 10000;
+
+  constructor(
+    nodeUrl1,
+    nodeUrl2?,
+    commitmentOrConfig?: Commitment | ConnectionConfig
+  ) {
+    this.nodeUrl = nodeUrl1;
+    this.commitmentOrConfig = commitmentOrConfig;
+    this.connection = new Connection(this.nodeUrl, this.commitmentOrConfig);
+    this.NODE_URL_1 = nodeUrl1;
+    this.NODE_URL_2 = nodeUrl2 || "https://solana-api.projectserum.com";
+    this.period = 100;
+    console.log(`Using RPC: ${this.nodeUrl}`);
+  }
+
+  backoff() {
+    console.log("Backing off");
+    if (this.period > this.MAX_NODE_THRESHOLD) {
+      if (this.nodeUrl == this.NODE_URL_1) {
+        this.nodeUrl = this.NODE_URL_2;
+      } else {
+        this.nodeUrl = this.NODE_URL_1;
+      }
+      this.connection = new Connection(this.nodeUrl, this.commitmentOrConfig);
+      console.log(`Switching to RPC node: ${this.nodeUrl}`);
+      this.period = 100;
+    }
+    sleep(this.period);
+    this.period *= 2;
+  }
+
+  getConfirmedSignaturesForAddress2(
+    address: PublicKey,
+    options?: ConfirmedSignaturesForAddress2Options,
+    commitment?: Finality
+  ): Promise<ConfirmedSignatureInfo[]> {
+    while (true) {
+      try {
+        return this.connection.getConfirmedSignaturesForAddress2(
+          address,
+          options,
+          commitment
+        );
+      } catch (error) {
+        console.error(
+          `<RPC> getConfirmedSignaturesForAddress2 failed with error ${error}`
+        );
+        this.backoff();
+      }
+    }
+  }
+
+  getParsedTransactions(
+    signatures: string[],
+    commitmentOrConfig?: Finality | GetVersionedTransactionConfig
+  ): Promise<ParsedTransactionWithMeta[]> {
+    while (true) {
+      try {
+        return this.connection.getParsedTransactions(
+          signatures,
+          commitmentOrConfig
+        );
+      } catch (error) {
+        console.error(`<RPC> getParsedTransactions failed with error ${error}`);
+        this.backoff();
+      }
+    }
+  }
+}
+
+// let connection = new Connection(process.env.RPC_URL, "finalized");
+
+let rpc = new SolanaRPC(
+  process.env.RPC_URL_1,
+  process.env.RPC_URL_2,
+  "finalized"
+);
+
+function spliceIntoChunks(arr, chunkSize) {
+  const res = [];
+  while (arr.length > 0) {
+    const chunk = arr.splice(0, chunkSize);
+    res.push(chunk);
+  }
+  return res;
+}
 
 export async function scrapeTransactionBatch(
   before: string,
@@ -21,14 +128,11 @@ export async function scrapeTransactionBatch(
   console.log(`Fetching all txs before ${before}`);
 
   // Note: this also grabs CPI calls
-  let sigInfos = await connection.getConfirmedSignaturesForAddress2(
-    PROGRAM_ID,
-    {
-      before: before,
-      until: until,
-      limit: MAX_SIGNATURE_BATCH_SIZE,
-    }
-  );
+  let sigInfos = await rpc.getConfirmedSignaturesForAddress2(PROGRAM_ID, {
+    before: before,
+    until: until,
+    limit: MAX_SIGNATURE_BATCH_SIZE,
+  });
   if (sigInfos.length == 0) {
     return {
       sig_len: 0,
@@ -38,23 +142,19 @@ export async function scrapeTransactionBatch(
   }
 
   let sigs = sigInfos.map((x) => x.signature);
+  let sig_chunks = spliceIntoChunks(sigs, MAX_TX_BATCH_SIZE);
+  let txs = (
+    await Promise.all(sig_chunks.map((s) => rpc.getParsedTransactions(s)))
+  ).flat();
 
-  let txs = await connection.getParsedTransactions(sigs);
-  // console.log(txs);
+  // txs = txs.filter((tx) => tx); // remove nulls
 
-  // let txs = await connection.getTransactions(sigs, {
-  //   commitment: "finalized",
-  //   maxSupportedTransactionVersion: 0,
-  // });
-
-  txs = txs.filter((tx) => tx); // remove nulls
-  
   // Chop off the straggler transactions from each end
   // We basically don't have a guarantee that those txes are in complete blocks
   // which can mess up due to non-deterministic intra-block ordering
-  txs = txs.filter(
-    (tx) => tx.slot !== txs[0].slot && tx.slot !== txs[txs.length - 1].slot
-  );
+  // txs = txs.filter(
+  //   (tx) => tx.slot !== txs[0].slot && tx.slot !== txs[txs.length - 1].slot
+  // );
   if (txs.length == 0) {
     return {
       sig_len: 0,
@@ -65,10 +165,10 @@ export async function scrapeTransactionBatch(
 
   let latestProcessedSig = txs[0].transaction.signatures[0];
   let earliestProcessedSig = txs[txs.length - 1].transaction.signatures[0];
-  console.log(`Fetched tx range (${txs.length} txs): 
+  console.log(`Fetched tx range (${txs.length} txs, ${sigInfos.length} sigs): 
   latest: ${latestProcessedSig} (${new Date(txs[0]?.blockTime * 1000)})
   earliest: ${earliestProcessedSig} (${new Date(
-    txs[sigs.length - 1]?.blockTime * 1000
+    txs[txs.length - 1]?.blockTime * 1000
   )})`);
 
   // if (!DEBUG_MODE) {
@@ -108,9 +208,10 @@ const indexingLoop = async () => {
           latestProcessedSig
         );
       }
-      if (r.sig_len < MAX_SIGNATURE_BATCH_SIZE) {
+      if (r.sig_len < MAX_SIGNATURE_BATCH_SIZE / 2) {
         console.log("%cNo more txs to scrape!", "color: green");
-        break;
+        // break;
+        return;
       }
     }
     // Wait until rescrape
@@ -118,11 +219,20 @@ const indexingLoop = async () => {
   }
 };
 
+export const refreshConnection = async () => {
+  rpc.connection = new Connection(rpc.nodeUrl, rpc.commitmentOrConfig);
+};
+
 const main = async () => {
   if (DEBUG_MODE) {
     console.log("Running in debug mode, will not push to AWS buckets");
   }
   indexingLoop();
+
+  setInterval(async () => {
+    console.log("%cRefreshing rpc connection", "color: cyan");
+    refreshConnection();
+  }, 1000 * 60 * 5); // Refresh every 5 minutes
 };
 
 main().catch(console.error.bind(console));
--- utils/constants.ts
+++ utils/constants.ts
@@ -1,5 +1,6 @@
 import { PublicKey } from "@solana/web3.js";
 
 export const PROGRAM_ID = new PublicKey(process.env.PROGRAM_ID);
-export const MAX_SIGNATURE_BATCH_SIZE = 200;
+export const MAX_SIGNATURE_BATCH_SIZE = 1000;
+export const MAX_TX_BATCH_SIZE = 200;
 export const DEBUG_MODE = process.argv.slice(2).includes("--debug");
