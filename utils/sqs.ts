bd45ed5d2a27c802940c36b46b2a9f7c7abaa536
tfrizza <tfrizza@gmail.com>
2022-10-12 11:29:33 +0800

    change console.log to console.error for aws functions

--- utils/sqs.ts
+++ utils/sqs.ts
@@ -2,18 +2,15 @@ import AWS from "aws-sdk";
 import { AWSOptions } from "./aws-config";
 
 let sqs = new AWS.SQS(AWSOptions);
-export const sendMessage = (
-    data: Array<string>,
-    queueName: string
-) => {
-    if (!data.length) return;
-    let messageData = { "data": data };
-    var params = {
-        MessageBody: JSON.stringify(messageData),
-        QueueUrl: queueName
-    };
-    sqs.sendMessage(params, function (err, data) {
-        if (err) console.log(err, err.stack); // an error occurred
-        else console.log(data);           // successful response
-    });
-}
+export const sendMessage = (data: Array<string>, queueName: string) => {
+  if (!data.length) return;
+  let messageData = { data: data };
+  var params = {
+    MessageBody: JSON.stringify(messageData),
+    QueueUrl: queueName,
+  };
+  sqs.sendMessage(params, function (err, data) {
+    if (err) console.error(err, err.stack); // an error occurred
+    else console.log(data); // successful response
+  });
+};
